01705da1ebae12f87613590e2d214612ada16504
nitgupta163 <niteshgupta0311@gmail.com>
2024-09-21 16:00:57 +0530

    Time: 20 ms (43.59%), Space: 13.8 MB (69.49%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,4 +1,6 @@
 class Solution {
+    /*
+    Time Complexity- O(nlogn)
     static bool comp(int a, int b)
     {
         string str1 = to_string(a);
@@ -7,13 +9,29 @@ class Solution {
         return true;
         return false;
     }
-public:
     vector<int> lexicalOrder(int n)
     {
         vector<int> ans(n);
         for(int i=1; i<=n; i++)
             ans[i-1] = i;
         sort(ans.begin(),ans.end(),comp);     
+        return ans;           
+    }
+    */
+    void dfs(int node, vector<int> &ans, int n)
+    {
+        if(node > n)
+        return ;
+        ans.push_back(node);
+        for(int i=0; i<=9; i++)
+            dfs(node*10+i,ans,n);
+    }
+public:
+    vector<int> lexicalOrder(int n)
+    {
+        vector<int> ans;
+        for(int i=1; i<=9; i++)
+            dfs(i,ans,n); 
         return ans;           
     }
 };
