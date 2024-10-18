945a70e001e0e1e3b93f7c19abcdbeecea9db4b0
nitgupta163 <niteshgupta0311@gmail.com>
2024-10-18 13:04:19 +0530

    Time: 123 ms (12.24%), Space: 309.1 MB (9.56%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,4 +1,47 @@
-class Solution {
+class Solution
+{
+    int countSubsetsRecursive(vector<int>& nums, int index, int currentOr,
+                              int targetOr, vector<vector<int>>& memo)
+    {
+        // Base case: reached the end of the array
+        if (index == nums.size()) {
+            return (currentOr == targetOr) ? 1 : 0;
+        }
+
+        // Check if the result for this state is already memoized
+        if (memo[index][currentOr] != -1) {
+            return memo[index][currentOr];
+        }
+
+        // Don't include the current number
+        int countWithout =
+            countSubsetsRecursive(nums, index + 1, currentOr, targetOr, memo);
+
+        // Include the current number
+        int countWith = countSubsetsRecursive(
+            nums, index + 1, currentOr | nums[index], targetOr, memo);
+
+        // Memoize and return the result
+        return memo[index][currentOr] = countWithout + countWith;
+    }
+public:
+    // Check editorital, several solutions there
+    int countMaxOrSubsets(vector<int>& nums)
+    {
+        int n = nums.size();
+        int maxOrValue = 0;
+
+        // Calculate the maximum OR value
+        for (int num : nums) {
+            maxOrValue |= num;
+        }
+
+        vector<vector<int>> memo(n, vector<int>(maxOrValue + 1, -1));
+
+        return countSubsetsRecursive(nums, 0, 0, maxOrValue, memo);
+    }
+};
+/*class Solution {
     void solve(int ind,vector<int>& nums, int &ans,
                const int &targetOrVal,int &currOrVal)
     {
@@ -23,3 +66,4 @@ public:
         return ans;   
     }
 };
+*/
