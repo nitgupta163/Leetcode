5ef9838788c705e164fb09f3ded6cefbdd92f1a3
nitgupta163 <niteshgupta0311@gmail.com>
2024-11-07 22:34:16 +0530

    Time: 42 ms (21.64%), Space: 60.2 MB (74.4%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,3 +1,32 @@
+/*
+Time complexity = O(N*Max(nums[i]))
+Result- TLE
+class Solution {
+    int solve(int ind, vector<int>& nums, int currAnd,
+                vector<unordered_map<int,int>> &dp)
+    {
+        if(ind >= nums.size())
+            return 0;
+        if(dp[ind].find(currAnd) != dp[ind].end())
+            return dp[ind][currAnd];
+        
+        int skip = solve(ind+1,nums,currAnd,dp);
+        int noSkip = 0;
+        if((nums[ind] & currAnd))
+            noSkip = 1+solve(ind+1,nums,(nums[ind] & currAnd),dp);
+        return dp[ind][currAnd] = max(skip,noSkip);
+    }
+public:
+    int largestCombination(vector<int>& nums)
+    {
+        int i, n = nums.size();
+        vector<unordered_map<int,int>> dp(n);
+        // inital value of currAnd should be all bits 111...
+        // which is greater than nums[i]
+        return solve(0,nums,(1<<30)-1,dp);
+    }
+};
+*/
 class Solution {
 public:
     int largestCombination(vector<int>& nums)
