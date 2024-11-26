b5a60bc3bd3b55dd94022c87f2664216073e470f
nitgupta163 <niteshgupta0311@gmail.com>
2024-11-26 21:50:19 +0530

    Time: 4 ms (72.43%), Space: 93.9 MB (66.24%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -3,7 +3,7 @@ public:
     int findChampion(int n, vector<vector<int>>& edges)
     {
         vector<int> dp(n,0);
-        for(auto e : edges)        
+        for(auto &e : edges)        
             dp[e[1]]++;
         int ans = -1;
         for(int i=0; i<n; i++)
